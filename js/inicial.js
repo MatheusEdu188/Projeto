f6f8779858f626157df3ffbda694c97813c09402
Matheus Eduardo <matheusedudu03@gmail.com>
2025-09-09 13:47:14 -0300

    Primeiro commit

---
+++ js/inicial.js
@@ -0,0 +1,88 @@
+const formImg = document.getElementById("formImg");
+const inputDescricao = document.getElementById("inputDescricao");
+const dataInput = document.getElementById("data");
+
+const publicacoes = document.querySelector(".publicacoes");
+const pubButton = document.getElementById("pubButton");
+
+let lista = JSON.parse(localStorage.getItem("publicacoes")) || [];
+
+
+function renderItem(item, prepend = false) {
+    const itemNovo = `
+        <img class="postimg" src="${item.arquivoSelecionado}" alt="img">
+        <p class="descricao">${item.descricao}</p>
+        <p class="data">${item.dataSelecionada}</p>
+    `;
+
+    const novaDiv = document.createElement("div");
+    novaDiv.classList.add("post");
+    novaDiv.innerHTML = itemNovo;
+
+    if (prepend) {
+        publicacoes.prepend(novaDiv);
+    } else {
+        publicacoes.appendChild(novaDiv);
+    }
+}
+
+
+lista.forEach(item => renderItem(item));
+
+
+pubButton.addEventListener("click", () => {
+    const descricao = inputDescricao.value;
+    const dataSelecionada = dataInput.value;
+    const arquivoSelecionado = formImg.files[0];
+
+    if (!descricao || !dataSelecionada || !arquivoSelecionado) {
+        alert("Preencha todos os campos antes de publicar!");
+        return;
+    }
+
+    
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        const imgBase64 = e.target.result;
+
+        const novoItem = {
+            descricao,
+            dataSelecionada,
+            arquivoSelecionado: imgBase64
+        };
+
+        lista.unshift(novoItem);
+
+        localStorage.setItem("publicacoes", JSON.stringify(lista));
+
+        renderItem(novoItem, true);
+
+        inputDescricao.value = "";
+        dataInput.value = "";
+        formImg.value = "";
+    };
+
+    reader.readAsDataURL(arquivoSelecionado);
+});
+
+
+
+
+
+
+
+
+
+
+const fecharbtn = document.querySelector(".fechar-btn")
+const modalcontainer = document.querySelector(".modal-container")
+const adcpost = document.querySelector("#adcpost")
+
+
+fecharbtn.addEventListener("click", ()=>{
+    modalcontainer.style.display = "none"
+})
+
+adcpost.addEventListener("click", ()=>{
+    modalcontainer.style.display = "flex"
+})
